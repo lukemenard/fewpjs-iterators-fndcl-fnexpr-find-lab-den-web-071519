4c0a094f77bbf9cc3c6b2254e27ef695f6d5661a
lukemenard <lukemenard@gmail.com>
2019-09-03 10:28:00 -0600

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
-const testVar = {}
+// const testVar = {}
+//
+// function testFunc() {
+//   return "hi"
+// }
+//
 
-function testFunc() {
-  return "hi"
+function superbowlWin(record) {
+  let win = record.find(function(game) {
+    return game.result === "W"
+  })
+  return win ? win.year : undefined
 }
